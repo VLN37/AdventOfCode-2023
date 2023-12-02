11361610ddd29366120c8b3a121c195bc422f174
VLN37 <jfpr1237@gmail.com>
2023-12-02 14:59:41 -0300

    day02 part2 ez

--- day_02/src/bin/part2.rs
+++ day_02/src/bin/part2.rs
@@ -1,3 +1,16 @@
+use day_02::Game;
+
 fn main() {
-    println!("hello day2");
+    let mut powers: Vec<u32> = Vec::new();
+
+    let input = include_str!("../../resources/input.txt");
+    for raw_game in input.lines() {
+        let game = Game::from(raw_game);
+        let red: u32 = game.bags.iter().map(|x| x.r).max().unwrap();
+        let green: u32 = game.bags.iter().map(|x| x.g).max().unwrap();
+        let blue: u32 = game.bags.iter().map(|x| x.b).max().unwrap();
+        powers.push(red * green * blue);
+    }
+    let result: u32 = powers.iter().sum();
+    println!("Result: {result}");
 }
