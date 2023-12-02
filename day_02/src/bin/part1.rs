408fa2a8acbff988d0e1b5c64cbd3bdebd5961ea
VLN37 <jfpr1237@gmail.com>
2023-12-02 14:35:10 -0300

    day02 part1

--- day_02/src/bin/part1.rs
+++ day_02/src/bin/part1.rs
@@ -1,3 +1,23 @@
+use day_02::{Bag, Game};
+
 fn main() {
-    println!("hello day2");
+    let magical_bag = Bag::factory(12, 13, 14);
+    let mut valid_games: Vec<usize> = Vec::new();
+
+    let input = include_str!("../../resources/input.txt");
+    for (id, raw_game) in input.lines().enumerate() {
+        let game = Game::from(raw_game);
+        let mut valid = true;
+        for bag in game.bags {
+            if !magical_bag.holds(&bag) {
+                valid = false;
+                break;
+            }
+        }
+        if valid {
+            valid_games.push(id + 1);
+        }
+    }
+    let result: usize = valid_games.iter().sum();
+    println!("Result: {result}");
 }
--- day_02/src/lib.rs
+++ day_02/src/lib.rs
@@ -1,5 +1,66 @@
+#[derive(Debug, Default)]
 pub struct Bag {
     pub r: u32,
     pub g: u32,
     pub b: u32,
+}
+
+impl Bag {
+    pub fn new() -> Self { Bag::default() }
+
+    pub fn factory(r: u32, g: u32, b: u32) -> Self { Bag { r, g, b } }
+
+    pub fn holds(&self, bag: &Bag) -> bool {
+        self.r >= bag.r && self.g >= bag.g && self.b >= bag.b
+    }
+}
+
+impl From<&str> for Bag {
+    fn from(raw: &str) -> Self {
+        let mut bag = Bag::new();
+        for balls in raw.split(',') {
+            let mut pair = balls.trim().split(' ');
+            let qty: u32 = pair.next().unwrap().parse().unwrap();
+            let color: &str = pair.next().unwrap().trim();
+            match color {
+                "red" => bag.r += qty,
+                "green" => bag.g += qty,
+                "blue" => bag.b += qty,
+                _ => panic!("invalid ball color"),
+            }
+        }
+        bag
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Game {
+    pub bags: Vec<Bag>,
+}
+
+impl Game {
+    pub fn new() -> Self { Game::default() }
+}
+
+impl From<&str> for Game {
+    fn from(raw: &str) -> Self {
+        let mut result = Game::new();
+        let game = raw.split(':').last().unwrap().trim().to_string();
+        for raw_bag in game.split(';') {
+            result.bags.push(Bag::from(raw_bag));
+        }
+        // dbg!(&result.bags);
+        result
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn day2p1() {
+        let bag = Bag::factory(12, 13, 14);
+        assert!(!bag.holds(&Bag::factory(13, 13, 13)));
+    }
 }
