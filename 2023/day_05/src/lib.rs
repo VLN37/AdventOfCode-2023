42b481b81b0f69c73fdb6ae6c58e1ff8356f00c1
VLN37 <jfpr1237@gmail.com>
2023-12-06 22:32:11 -0300

    day05 part1

--- 2023/day_05/src/lib.rs
+++ 2023/day_05/src/lib.rs
@@ -1,15 +1,15 @@
 #[derive(Debug, Default)]
 pub struct Instructions {
-    pub src: usize,
-    pub dst: usize,
-    pub n:   usize,
+    pub dst: i64,
+    pub src: i64,
+    pub n:   i64,
 }
 
 impl From<&str> for Instructions {
     /// ### Format example
     /// "1 2 3"
     fn from(value: &str) -> Self {
-        let mut iter = value.split(' ').map(|x| x.parse::<usize>().unwrap());
+        let mut iter = value.split(' ').map(|x| x.parse::<i64>().unwrap());
         Instructions {
             dst: iter.next().unwrap(),
             src: iter.next().unwrap(),
@@ -17,16 +17,3 @@ impl From<&str> for Instructions {
         }
     }
 }
-
-// impl<'a> FromIterator<&'a str> for Instructions {
-//     fn from_iter<T: IntoIterator<Item = &'a str>>(iter: T) -> Self {
-//         Vec::<Instructions>::from_iter(iter)
-//     }
-// }
-// seed - soil
-// soil - fert
-// fert - water
-// water - light
-// light - temp
-// temp - humidity
-// humidity - location
