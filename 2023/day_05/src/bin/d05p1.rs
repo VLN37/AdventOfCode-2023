184ac1a30fbe127577ea6ca1e18e86219afe4d18
VLN37 <jfpr1237@gmail.com>
2023-12-06 22:38:05 -0300

    day05 part1 cleanup

--- 2023/day_05/src/bin/d05p1.rs
+++ 2023/day_05/src/bin/d05p1.rs
@@ -1,15 +1,7 @@
 use day_05::Instructions;
 
-// seed - soil
-// soil - fert
-// fert - water
-// water - light
-// light - temp
-// temp - humidity
-// humidity - location
-
 fn main() {
-    let input = include_str!("../../resources/input.txt");
+    let input = include_str!("../../resources/small_input.txt");
     let seeds: Vec<i64> = input[..input.find("\n\n").unwrap()]
         .split(':')
         .last()
@@ -26,22 +18,20 @@ fn main() {
         .join("\n");
     clean.pop();
     let mut instructions: Vec<Vec<Instructions>> = Vec::new();
-    for (i, value) in clean.split("\n\n").enumerate() {
-        instructions.insert(i, value.split('\n').map(Instructions::from).collect());
+    for value in clean.split("\n\n") {
+        instructions.push(value.split('\n').map(Instructions::from).collect());
     }
     let mut results: Vec<i64> = Vec::new();
-    for seed in seeds {
-        let mut n = seed;
+    for mut seed in seeds {
         for vec in instructions.iter() {
             for line in vec {
-                if n >= line.src && n < line.src + line.n {
-                    n = line.dst + n - line.src;
-                    // println!("new {n}");
+                if seed >= line.src && seed < line.src + line.n {
+                    seed += line.dst - line.src;
                     break;
                 }
             }
         }
-        results.push(n);
+        results.push(seed);
     }
     println!("Result: {}", results.iter().min().unwrap());
 }
