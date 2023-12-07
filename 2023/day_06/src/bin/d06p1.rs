0745c531f73c1bd4455b5ac7deb94c693c904f55
VLN37 <jfpr1237@gmail.com>
2023-12-07 00:47:04 -0300

    day06 part1 ez

--- 2023/day_06/src/bin/d06p1.rs
+++ 2023/day_06/src/bin/d06p1.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello World!");
+    let input = include_str!("../../resources/input.txt");
+    let mut input = input.lines().map(|x| x.split(':').last().unwrap());
+
+    let t = input.next().unwrap();
+    let d = input.next().unwrap();
+
+    let mut time: Vec<usize> = Vec::new();
+    time.extend(t.split_whitespace().map(|x| x.parse::<usize>().unwrap()));
+    let mut distance: Vec<usize> = Vec::new();
+    distance.extend(d.split_whitespace().map(|x| x.parse::<usize>().unwrap()));
+
+    let mut results = Vec::new();
+    for (&t, &d) in time.iter().zip(distance.iter()) {
+        let mut n = 0;
+        for i in 0..t {
+            if (t - i) * i > d {
+                n += 1;
+            }
+        }
+        results.push(n);
+    }
+    println!("Result: {}", results.iter().product::<usize>());
 }
