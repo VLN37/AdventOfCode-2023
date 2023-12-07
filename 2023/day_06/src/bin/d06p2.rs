699947cd88e9dedd713d10eb22d3ada0dcadf995
VLN37 <jfpr1237@gmail.com>
2023-12-07 00:54:25 -0300

    day06 part2

--- 2023/day_06/src/bin/d06p2.rs
+++ 2023/day_06/src/bin/d06p2.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello World!");
+    let input = include_str!("../../resources/input.txt");
+    let mut input = input
+        .lines()
+        .map(|x| x.split(':').last().unwrap())
+        .map(|x| x.replace(' ', ""));
+
+    let time = input.next().unwrap().parse::<usize>().unwrap();
+    let distance = input.next().unwrap().parse::<usize>().unwrap();
+
+    let mut n = 0;
+    for i in 0..time {
+        if (time - i) * i > distance {
+            n += 1;
+        }
+    }
+    println!("Result: {}", n);
 }
