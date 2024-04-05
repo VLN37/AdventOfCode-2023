5d2fa93eaf1bd85af6a88ffe11f56509c78aa78f
VLN37 <jfpr1237@gmail.com>
2024-04-04 22:05:35 -0300

    day10 happy path

--- 2023/day_10/src/bin/d10p1.rs
+++ 2023/day_10/src/bin/d10p1.rs
@@ -1,4 +1,50 @@
+use day_10::*;
+
 fn main() {
-    include_str!("../../resources/small_input.txt");
-    println!("Hello World!");
+    let input = include_str!("../../resources/input.txt");
+    let mut maze: Vec<Vec<char>> =
+        input.lines().map(|x| Vec::from_iter(x.chars())).collect();
+
+    let (mut x, mut y) = find_player(&maze);
+    let init_pos = (x, y);
+
+    let mut distance: usize = 0;
+    let mut steps: Vec<(usize, usize)> = Vec::new();
+
+    dbg!(init_pos, x, y);
+    loop {
+        if valid_move(Cardinal::North, maze[x + 1][y]) {
+            if (x, y) != init_pos {
+                maze[x][y] = 'x';
+            }
+            x += 1;
+        } else if valid_move(Cardinal::South, maze[x - 1][y]) {
+            if (x, y) != init_pos {
+                maze[x][y] = 'x';
+            }
+            x -= 1;
+        } else if valid_move(Cardinal::East, maze[x][y + 1]) {
+            if (x, y) != init_pos {
+                maze[x][y] = 'x';
+            }
+            y += 1;
+        } else if valid_move(Cardinal::West, maze[x][y - 1]) {
+            if (x, y) != init_pos {
+                maze[x][y] = 'x';
+            }
+            y -= 1;
+        } else {
+            panic!("invalid move");
+        }
+        distance += 1;
+        steps.push((x, y));
+        dbg!(maze[x][y]);
+        dbg!(x, y);
+        if (x, y) == init_pos {
+            println!("exit found");
+            break;
+        }
+    }
+    dbg!(distance);
+    dbg!(steps);
 }
--- 2023/day_10/src/lib.rs
+++ 2023/day_10/src/lib.rs
@@ -1,1 +1,34 @@
+pub fn find_player(vec: &[Vec<char>]) -> (usize, usize) {
+    let mut x: usize = 0;
+    let mut y: usize = 0;
+    let mut found = false;
 
+    for (i, line) in vec.iter().enumerate() {
+        if let Some(idx) = line.iter().position(|x| *x == 'S') {
+            x = i;
+            y = idx;
+            found = true;
+            break;
+        }
+    }
+    if !found {
+        panic!("char not found");
+    }
+    (x, y)
+}
+
+pub enum Cardinal {
+    East,
+    West,
+    North,
+    South,
+}
+
+pub fn valid_move(direction: Cardinal, c: char) -> bool {
+    match direction {
+        Cardinal::North => "|LJS".contains(c),
+        Cardinal::West => "7J-S".contains(c),
+        Cardinal::East => "FL-JS".contains(c),
+        Cardinal::South => "|7FS".contains(c),
+    }
+}
