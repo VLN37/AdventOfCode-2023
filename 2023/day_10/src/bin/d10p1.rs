c9128d0de624a4ce0d9d29e3d103264e2f39ab78
VLN37 <jfpr1237@gmail.com>
2024-04-07 15:50:50 -0300

    overflow cleared

--- 2023/day_10/src/bin/d10p1.rs
+++ 2023/day_10/src/bin/d10p1.rs
@@ -1,26 +1,26 @@
 use day_10::*;
 
 fn recurse(
-    maze: &mut [Vec<char>],
+    maze: &mut Vec<Vec<char>>,
     steps: &mut Vec<(usize, usize)>,
     mut x: usize,
     mut y: usize,
     distance: usize,
 ) -> bool {
     let mut viable = false;
-    dbg!(maze[x][y]);
-    dbg!(maze
-        .to_owned()
-        .iter()
-        .map(|x| x.to_owned().iter().collect::<String>())
-        .collect::<Vec<String>>());
-    dbg!(&distance);
+    // dbg!(maze[x][y]);
+    // dbg!(maze
+    // .to_owned()
+    // .iter()
+    // .map(|x| x.to_owned().iter().collect::<String>())
+    // .collect::<Vec<String>>());
+    println!("{distance}");
     if valid_move(Cardinal::South, maze[x][y], maze[x + 1][y]) {
         if maze[x][y] != 'S' {
             maze[x][y] = 'x';
         }
-        dbg!("recurse", maze[x][y]);
-        dbg!('&');
+        // dbg!("recurse", maze[x][y]);
+        // dbg!('&');
         x += 1;
         viable = recurse(maze, steps, x, y, distance + 1);
         if !viable {
@@ -30,8 +30,8 @@ fn recurse(
         if maze[x][y] != 'S' {
             maze[x][y] = 'x';
         }
-        dbg!("recurse", maze[x][y]);
-        dbg!('&');
+        // dbg!("recurse", maze[x][y]);
+        // dbg!('&');
         x -= 1;
         viable = recurse(maze, steps, x, y, distance + 1);
         if !viable {
@@ -41,8 +41,8 @@ fn recurse(
         if maze[x][y] != 'S' {
             maze[x][y] = 'x';
         }
-        dbg!("recurse", maze[x][y]);
-        dbg!('&');
+        // dbg!("recurse", maze[x][y]);
+        // dbg!('&');
         y += 1;
         viable = recurse(maze, steps, x, y, distance + 1);
         if !viable {
@@ -52,8 +52,8 @@ fn recurse(
         if maze[x][y] != 'S' {
             maze[x][y] = 'x';
         }
-        dbg!("recurse", maze[x][y]);
-        dbg!('&');
+        // dbg!("recurse", maze[x][y]);
+        // dbg!('&');
         y -= 1;
         viable = recurse(maze, steps, x, y, distance + 1);
         if !viable {
@@ -61,12 +61,12 @@ fn recurse(
         }
     }
     if viable {
-        dbg!("push", (x, y), maze[x][y]);
+        // dbg!("push", (x, y), maze[x][y]);
         steps.push((x, y));
         return true;
     }
     if !viable && back_to_where_i_started(maze, x, y) {
-        dbg!("push end", (x, y), maze[x][y]);
+        // dbg!("push end", (x, y), maze[x][y]);
         maze[x][y] = 'x';
         return true;
     }
