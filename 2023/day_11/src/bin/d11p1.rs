d410005cc3e6f186aa8c712273efd6a4a6a3ada2
VLN37 <jfpr1237@gmail.com>
2024-04-09 20:25:50 -0300

    day11p1

--- 2023/day_11/src/bin/d11p1.rs
+++ 2023/day_11/src/bin/d11p1.rs
@@ -1,4 +1,25 @@
+use day_11::Universe;
+
 fn main() {
-    include_str!("../../resources/small_input.txt");
-    println!("Hello World!");
+    let input = include_str!("../../resources/input.txt");
+    let mut uni = Universe::from(input);
+    uni.expand();
+    println!("expand");
+    let mut galaxies = uni.galaxies();
+    let clone = galaxies.clone();
+    println!("clone");
+    // dbg!(&galaxies);
+    for g in galaxies.iter_mut() {
+        println!("g");
+        g.distances = clone
+            .iter()
+            .map(|x| (g.y - x.y).abs() + (g.x - x.x).abs())
+            .collect();
+        dbg!(g.id, g.x, g.y, &g.distances);
+    }
+    let sum: i64 = galaxies
+        .iter()
+        .map(|x| x.distances.iter().sum::<i64>())
+        .sum();
+    dbg!(sum / 2);
 }
--- 2023/day_11/src/lib.rs
+++ 2023/day_11/src/lib.rs
@@ -1,1 +1,78 @@
+#[derive(Default, Clone)]
+pub struct Galaxy {
+    pub id:        usize,
+    pub x:         i64,
+    pub y:         i64,
+    pub distances: Vec<i64>,
+}
 
+pub struct Universe {
+    pub uni: Vec<Vec<char>>,
+}
+
+const EMPTY: char = '.';
+const GALAXY: char = '#';
+
+impl Universe {
+    pub fn expand(&mut self) {
+        for (i, line) in self.uni.clone().iter().enumerate().rev() {
+            if line.iter().all(|x| *x == '.') {
+                self.uni.insert(i, line.clone());
+            }
+        }
+        for i in (0..self.uni.first().unwrap().len()).rev() {
+            if self.uni.iter().map(|x| x[i]).all(|x| x == EMPTY) {
+                for line in &mut self.uni {
+                    line.insert(i, EMPTY);
+                }
+            }
+        }
+    }
+
+    pub fn galaxies(&self) -> Vec<Galaxy> {
+        let mut galaxies: Vec<Galaxy> = Vec::new();
+        let mut id = 1;
+        for x in 0..self.height() {
+            // dbg!(self.length());
+            for y in 0..self.length() {
+                // dbg!(self.height());
+                if self.uni[x][y] == GALAXY {
+                    galaxies.push(Galaxy {
+                        x: x as i64,
+                        y: y as i64,
+                        id,
+                        ..Default::default()
+                    });
+                    id += 1;
+                }
+            }
+        }
+        galaxies
+    }
+
+    pub fn length(&self) -> usize { self.uni.first().unwrap().len() }
+
+    pub fn height(&self) -> usize { self.uni.len() }
+
+    pub fn dump(&self) {
+        println!(
+            "dimensions: x={} y={}",
+            self.uni.len(),
+            self.uni.first().unwrap().len()
+        );
+        let dump: Vec<String> = self
+            .uni
+            .iter()
+            .map(|x| String::from_iter(x.iter()))
+            .collect();
+        dbg!(dump);
+    }
+}
+
+impl From<&str> for Universe {
+    fn from(input: &str) -> Self {
+        Universe {
+            uni: input.lines().map(|x| x.chars().collect()).collect(),
+        }
+    }
+}
